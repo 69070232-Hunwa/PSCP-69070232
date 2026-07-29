8a22468ccef4bac11a9fc2328fbe5c04d8078e88
69070232-Hunwa <69070232@kmitl.ac.th>
2026-07-30 00:39:37 +0700

    Create 3042.py

---
+++ 3042/3042.py
@@ -0,0 +1,10 @@
+"""oirhfoirhgf"""
+
+def main():
+    """iorho"""
+    num = int(input())
+    st = num - (num % 10)
+    result = [str(i) for i in range(st, -1 , -10)]
+    print(" ".join(result))
+
+main()
